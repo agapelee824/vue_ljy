d22f38878cf7f187dcce26a4301a704576e492d2
LJY <agapelee824@gmail.com>
2023-11-10 10:47:21 +0900

    create

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createWebHistory, createRouter } from 'vue-router'
+import Login from '@/components/Login'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Login',
+        component: Login
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
