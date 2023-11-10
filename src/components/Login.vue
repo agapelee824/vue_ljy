1e805511060bc083a8ed302cb9cf339f1ecca683
LJY <agapelee824@gmail.com>
2023-11-10 15:53:06 +0900

    JWT Security

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -39,8 +39,28 @@ export default {
         alert('비밀번호를 입력하세요.')
         return
       }
-
-      alert('로그인 되었습니다.')
+      
+      console.log(this.user_id + ":" + this.user_pw)
+      this.$axios({
+        //url: 'http://localhost:8081/auth', // 통신할 웹문서
+        url: 'http://localhost:8081/api/member/login',
+        method: 'post', // 통신 방식
+        Headers: {
+          "Content-type": "multipart/form-date"
+        },
+        data: { // 보낼 데이터
+          //foo: 'hi'
+          loginId: this.user_id,
+          password: this.user_pw
+        }
+      }).then(res => {
+        console.log(res)
+        alert('로그인 되었습니다.')
+      }).catch(error => {
+        console.log(error)
+      }).finally(() => {
+        console.log("finally")
+      })
     }
   }
 }
